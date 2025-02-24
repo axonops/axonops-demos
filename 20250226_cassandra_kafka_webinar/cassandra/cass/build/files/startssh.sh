179ea499bc98ccdc8d8280ac1a6ee58895dd2b80
Johnny Miller <johnny.p.miller@gmail.com>
2025-02-24 12:33:47 +0100

    stuff

---
+++ 20250226_cassandra_kafka_webinar/cassandra/cass/build/files/startssh.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -e
+
+logit() {
+    local logdate=$(date +"%Y%m%d-%H:%M:%S %Z")
+    local message="[${logdate}] - ${*}"
+    echo "${message}"
+    logger "${message}"
+}
+eexit() {
+    local error_str="$@"
+    logit $error_str
+    exit 1
+}
+
+logit "Starting SSH service..."
+# Start the SSH daemon; by default, sshd will fork to the background
+/usr/sbin/sshd
+
+# Verify that the SSH daemon started successfully
+if pgrep sshd > /dev/null; then
+  logit "SSH service is running."
+else
+  eexit "Failed to start SSH service!"
+fi
